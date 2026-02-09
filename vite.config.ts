aa7a56f1d7597c90956578ce25f3a060d5e29c50
kekekuli <kekekuliobj@gmail.com>
2026-02-09 15:44:03 +0800

    add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
+import { analyzer } from 'vite-bundle-analyzer'
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), process.env.ANALYZE === "true" && analyzer()],
 });
